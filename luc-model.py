b377aead730ef009200e50e5bcc11deb88e1eef9
DBlev886 <106088058+DBlev886@users.noreply.github.com>
2022-09-17 09:45:42 +0800

    detect land cover in study area

---
+++ luc-model.py
@@ -0,0 +1,296 @@
+# %%
+# Jupyter notebook related
+%reload_ext autoreload
+%autoreload 2
+%matplotlib inline
+
+import datetime
+import itertools
+
+# Built-in modules
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
+
+# Basics of Python data handling and visualization
+import numpy as np
+from aenum import MultiValueEnum
+
+np.random.seed(42)
+import geopandas as gpd
+import joblib
+
+# Machine learning
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm, ListedColormap
+from shapely.geometry import Polygon
+from sklearn import metrics, preprocessing
+from tqdm.auto import tqdm
+
+from sentinelhub import DataCollection, UtmZoneSplitter
+
+# Imports from eo-learn and sentinelhub-py
+from eolearn.core import (
+    EOExecutor,
+    EOPatch,
+    EOTask,
+    EOWorkflow,
+    FeatureType,
+    LoadTask,
+    MergeFeatureTask,
+    OverwritePermission,
+    SaveTask,
+    linearly_connect_tasks,
+)
+from eolearn.features import LinearInterpolationTask, NormalizedDifferenceIndexTask, SimpleFilterTask
+from eolearn.geometry import ErosionTask, VectorToRasterTask
+from eolearn.io import ExportToTiffTask, SentinelHubInputTask, VectorImportTask
+from eolearn.ml_tools import FractionSamplingTask
+
+
+# %% >>> Get country boundary
+# Folder where data for running the notebook is stored
+DATA_FOLDER = os.path.join("..", "..", "C:/Users/Danny/Desktop/EMMA/IV/Data/")
+# Locations for collected data and intermediate results
+EOPATCH_FOLDER = os.path.join(".", "eopatches")
+EOPATCH_SAMPLES_FOLDER = os.path.join(".", "eopatches_sampled")
+RESULTS_FOLDER = os.path.join(".", "results")
+for folder in (EOPATCH_FOLDER, EOPATCH_SAMPLES_FOLDER, RESULTS_FOLDER):
+    os.makedirs(folder, exist_ok=True)
+
+
+# Load geojson file
+country = gpd.read_file(os.path.join(DATA_FOLDER, "aoi.shp"))
+# Add 500m buffer to secure sufficient data near border
+country = country.buffer(500)
+
+# Get the country's shape in polygon format
+country_shape = country.geometry.values[0]
+
+# Plot country
+country.plot()
+plt.axis("off")
+
+# Print size
+country_width = country_shape.bounds[2] - country_shape.bounds[0]
+country_height = country_shape.bounds[3] - country_shape.bounds[1]
+print(f"Dimension of the area is {country_width:.0f} x {country_height:.0f} m2")
+
+
+# %% >>> Split to smaller tiles and choose a 5x5 area
+# Create a splitter to obtain a list of bboxes with 5km sides
+bbox_splitter = UtmZoneSplitter([country_shape], country.crs, 5000)
+
+bbox_list = np.array(bbox_splitter.get_bbox_list())
+info_list = np.array(bbox_splitter.get_info_list())
+
+# Prepare info of selected EOPatches
+geometry = [Polygon(bbox.get_polygon()) for bbox in bbox_list]
+idxs = [info["index"] for info in info_list]
+idxs_x = [info["index_x"] for info in info_list]
+idxs_y = [info["index_y"] for info in info_list]
+
+bbox_gdf = gpd.GeoDataFrame({"index": idxs, "index_x": idxs_x, "index_y": idxs_y}, crs=country.crs, geometry=geometry)
+
+# select a 5x5 area (id of center patch)
+ID = 150
+
+# Obtain surrounding 5x5 patches
+patchIDs = []
+for idx, (bbox, info) in enumerate(zip(bbox_list, info_list)):
+    if abs(info["index_x"] - info_list[ID]["index_x"]) <= 2 and abs(info["index_y"] - info_list[ID]["index_y"]) <= 2:
+        patchIDs.append(idx)
+
+# Check if final size is 5x5
+if len(patchIDs) != 50 * 50:
+    print("Warning! Use a different central patch ID, this one is on the border.")
+
+# Change the order of the patches (useful for plotting)
+patchIDs = np.transpose(np.fliplr(np.array(patchIDs).reshape(50, 50))).ravel()
+
+# Save to shapefile
+shapefile_name = "grid_beni_100x100.gpkg"
+bbox_gdf.to_file(os.path.join(RESULTS_FOLDER, shapefile_name), driver="GPKG")
+
+# Display bboxes over country
+fig, ax = plt.subplots(figsize=(30, 30))
+ax.set_title("Selected 5x5 tiles from Slovenia", fontsize=25)
+country.plot(ax=ax, facecolor="w", edgecolor="b", alpha=0.5)
+bbox_gdf.plot(ax=ax, facecolor="w", edgecolor="r", alpha=0.5)
+
+for bbox, info in zip(bbox_list, info_list):
+    geo = bbox.geometry
+    ax.text(geo.centroid.x, geo.centroid.y, info["index"], ha="center", va="center")
+
+# Mark bboxes of selected area
+bbox_gdf[bbox_gdf.index.isin(patchIDs)].plot(ax=ax, facecolor="g", edgecolor="r", alpha=0.5)
+
+plt.axis("off");
+
+
+# %% >>> Define some needed custom EOTasks
+class SentinelHubValidDataTask(EOTask):
+    """
+    Combine Sen2Cor's classification map with `IS_DATA` to define a `VALID_DATA_SH` mask
+    The SentinelHub's cloud mask is asumed to be found in eopatch.mask['CLM']
+    """
+
+    def __init__(self, output_feature):
+        self.output_feature = output_feature
+
+    def execute(self, eopatch):
+        eopatch[self.output_feature] = eopatch.mask["IS_DATA"].astype(bool) & (~eopatch.mask["CLM"].astype(bool))
+        return eopatch
+
+
+class AddValidCountTask(EOTask):
+    """
+    The task counts number of valid observations in time-series and stores the results in the timeless mask.
+    """
+
+    def __init__(self, count_what, feature_name):
+        self.what = count_what
+        self.name = feature_name
+
+    def execute(self, eopatch):
+        eopatch[FeatureType.MASK_TIMELESS, self.name] = np.count_nonzero(eopatch.mask[self.what], axis=0)
+        return eopatch
+
+
+# %% >>> Define the workflow tasks
+# BAND DATA
+# Add a request for S2 bands.
+# Here we also do a simple filter of cloudy scenes (on tile level).
+# The s2cloudless masks and probabilities are requested via additional data.
+band_names = ["B02", "B03", "B04", "B08", "B11", "B12"]
+add_data = SentinelHubInputTask(
+    bands_feature=(FeatureType.DATA, "BANDS"),
+    bands=band_names,
+    resolution=10,
+    maxcc=0.8,
+    time_difference=datetime.timedelta(minutes=120),
+    data_collection=DataCollection.SENTINEL2_L1C,
+    additional_data=[(FeatureType.MASK, "dataMask", "IS_DATA"), (FeatureType.MASK, "CLM"), (FeatureType.DATA, "CLP")],
+    max_threads=5,
+)
+
+
+# CALCULATING NEW FEATURES
+# NDVI: (B08 - B04)/(B08 + B04)
+# NDWI: (B03 - B08)/(B03 + B08)
+# NDBI: (B11 - B08)/(B11 + B08)
+ndvi = NormalizedDifferenceIndexTask(
+    (FeatureType.DATA, "BANDS"), (FeatureType.DATA, "NDVI"), [band_names.index("B08"), band_names.index("B04")]
+)
+ndwi = NormalizedDifferenceIndexTask(
+    (FeatureType.DATA, "BANDS"), (FeatureType.DATA, "NDWI"), [band_names.index("B03"), band_names.index("B08")]
+)
+ndbi = NormalizedDifferenceIndexTask(
+    (FeatureType.DATA, "BANDS"), (FeatureType.DATA, "NDBI"), [band_names.index("B11"), band_names.index("B08")]
+)
+
+
+# VALIDITY MASK
+# Validate pixels using SentinelHub's cloud detection mask and region of acquisition
+add_sh_validmask = SentinelHubValidDataTask((FeatureType.MASK, "IS_VALID"))
+
+# COUNTING VALID PIXELS
+# Count the number of valid observations per pixel using valid data mask
+add_valid_count = AddValidCountTask("IS_VALID", "VALID_COUNT")
+
+# SAVING TO OUTPUT (if needed)
+save = SaveTask(EOPATCH_FOLDER, overwrite_permission=OverwritePermission.OVERWRITE_PATCH)
+
+
+# %% >>> Reference map task
+class LULC(MultiValueEnum):
+    """Enum class containing basic LULC types"""
+
+    NO_DATA = "No Data", 0, "#ffffff"
+    CULTIVATED_LAND = "Cultivated Land", 1, "#ffff00"
+    FOREST = "Forest", 2, "#054907"
+    GRASSLAND = "Grassland", 3, "#ffa500"
+    SHRUBLAND = "Shrubland", 4, "#806000"
+    WATER = "Water", 5, "#069af3"
+    WETLAND = "Wetlands", 6, "#95d0fc"
+    TUNDRA = "Tundra", 7, "#967bb6"
+    ARTIFICIAL_SURFACE = "Artificial Surface", 8, "#dc143c"
+    BARELAND = "Bareland", 9, "#a6a6a6"
+    SNOW_AND_ICE = "Snow and Ice", 10, "#000000"
+
+    @property
+    def id(self):
+        return self.values[1]
+
+    @property
+    def color(self):
+        return self.values[2]
+
+
+# Reference colormap things
+lulc_cmap = ListedColormap([x.color for x in LULC], name="lulc_cmap")
+lulc_norm = BoundaryNorm([x - 0.5 for x in range(len(LULC) + 1)], lulc_cmap.N)
+
+
+land_use_ref_path = os.path.join(DATA_FOLDER, "/Land_cover/data/LandCover_NP_2019.tif")
+
+vector_feature = FeatureType.VECTOR_TIMELESS, "LULC_REFERENCE"
+
+vector_import_task = VectorImportTask(vector_feature, land_use_ref_path)
+
+rasterization_task = VectorToRasterTask(
+    vector_feature,
+    (FeatureType.MASK_TIMELESS, "LULC"),
+    values_column="lulcid",
+    raster_shape=(FeatureType.MASK, "IS_DATA"),
+    raster_dtype=np.uint8,
+)
+
+
+# %% >>> Define the workflow
+
+# Define the workflow
+workflow_nodes = linearly_connect_tasks(
+    add_data, ndvi, ndwi, ndbi, add_sh_validmask, add_valid_count, vector_import_task, rasterization_task, save
+)
+workflow = EOWorkflow(workflow_nodes)
+
+# Let's visualize it
+workflow.dependency_graph()
+
+
+# %% >>>
+%%time
+
+# Time interval for the SH request
+time_interval = ["2019-01-01", "2019-12-31"]
+
+# Define additional parameters of the workflow
+input_node = workflow_nodes[0]
+save_node = workflow_nodes[-1]
+execution_args = []
+for idx, bbox in enumerate(bbox_list[patchIDs]):
+    execution_args.append(
+        {
+            input_node: {"bbox": bbox, "time_interval": time_interval},
+            save_node: {"eopatch_folder": f"eopatch_{idx}"},
+        }
+    )
+
+# Execute the workflow
+executor = EOExecutor(workflow, execution_args, save_logs=True)
+executor.run(workers=4)
+
+executor.make_report()
+
+failed_ids = executor.get_failed_executions()
+if failed_ids:
+    raise RuntimeError(
+        f"Execution failed EOPatches with IDs:\n{failed_ids}\n"
+        f"For more info check report at {executor.get_report_path()}"
+    )
+
+
+# %% >>> Visualize the patches
+EOPatch.load('./eopatches/eopatch_0/')
+
